cc238abba4427ff92a8b5b3ea614764323279183
Kajal-Abacus <kajalabacus99@gmail.com>
2023-01-29 14:23:39 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,163 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+   question: "What is the smallest header in HTML by default?",
+    answer: "h6",
+    options: [
+      "h5",
+      "h6",
+      "h1",
+      "h2"
+    ]
+  },
+   
+  {
+    numb: 2,
+    question: "Which of the following function of String object returns the characters in a string between two indexes into the string?",
+    answer: "substring()",
+    options: [
+      "slice()",
+      "split()",
+      "substr()",
+      "substring()"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
+    answer: "anchor()",
+    options: [
+      "anchor()",
+      "link()",
+      "blink()",
+      "big()"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following function of Array object represents the source code of an object?",
+    answer: "toSource()",
+    options: [
+      "toSource()",
+      "splice()",
+      "toString()",
+      "unshift()"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which built-in method returns the calling string value converted to lower case?",
+    answer: "toLowerCase()",
+    options: [
+      "toLowerCase()",
+      "toLower()",
+      "changeCase(case)",
+      "None of the above."
+    ]
+  },
+   {
+     numb: 6,
+    question: "What does HTML stand for?",
+     answer: "Hyper Text Markup Language",
+     options: [
+       "Hyper Text Preprocessor",
+       "Hyper Text Markup Language",
+       "Hyper Text Multiple Language",
+       "Hyper Tool Multi Language"
+     ]
+   },
+   
+  {
+    numb: 7,
+   question: " Arrays in JavaScript are defined by which of the following statements?",
+    answer: "It is an ordered list of values",
+    options: [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions"
+    ]
+  },
+  {
+    numb: 8,
+   question: "Which of the following is correct about JavaScript?",
+    answer: "JavaScript is an Object-Based language",
+    options: [
+      "JavaScript is an Object-Based language",
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language",
+      "JavaScript is a High-level language"
+    ]
+  },
+  
+  {
+    numb: 9,
+   question: " The correct sequence of HTML tags for starting a webpage is ?",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Body, Title",
+      "HTML, Head, Title, Body"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is correct about features of JavaScript?",
+    answer: "All of the above.",
+    options: [
+      "JavaScript is a lightweight, interpreted programming language",
+      "JavaScript is designed for creating network-centric applications",
+      "JavaScript is complementary to and integrated with Java.",
+      "All of the above."
+    ]
+  },
+ 
+  //{
+   // numb: 11,
+   //question: "What does HTML stand for?",
+   // answer: "Hyper Text Markup Language",
+   // options: [
+    //  "Hyper Text Preprocessor",
+    //  "Hyper Text Markup Language",
+    //  "Hyper Text Multiple Language",
+    //  "Hyper Tool Multi Language"
+    //]
+  //},
+  //{
+   // numb: 12,
+  // question: "How many sizes of headers are available in HTML by default?",
+   // answer: "6",
+   // options: [
+     // "5",
+     // "1",
+     // "3",
+     // "6"
+    //]
+  //},
+  //{
+    //numb: 13,
+   //question: "Which of the following is not javascript data types?",
+   // answer: "All of the mentioned",
+    //options: [
+      //"Null type",
+      //"Undefined type",
+      //"Number type",
+     // "All of the mentioned"
+   // ]
+  //},
+  //{
+    //numb: 14,
+   //question: "Which of the following can be used to call a JavaScript Code Snippet?",
+   // answer: "Function/Method",
+    //options: [
+     // "Preprocessor",
+     // "Function/Method",
+     // "RMI",
+     // "Triggering Event"
+    //]
+  //},
+  
+
+];
